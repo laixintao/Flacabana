349a6da46f3a0abc87fa39ccbae8942b5bb8fcc3
赖信涛 <laixintao1995@163.com>
2015-09-08 14:30:51 +0800

    use templates

--- app/views.py
+++ app/views.py
@@ -1,8 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
-@app.route('/')
+@app.route('/index')
 def index():
-    return "hello,world"
-
-
+    user = {'nickname':'Miguel'} # fake user
+    posts = [
+            {
+                'author':{'nickname':'john'},
+                'body':'Beautiful day in Portland!'
+            },
+            {
+                'author':{'nickname':'Susan'},
+                'body':'The Avengers movie was so cool!'
+            }
+        ]
+    return render_template('index.html',
+            title='Home',
+            user=user,
+            posts=posts)
