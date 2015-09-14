0323a6f4812be46adee223b876969cff092297a1
赖信涛 <laixintao1995@163.com>
2015-09-14 23:58:38 +0800

    add sqlite config

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 OPENID_PROVIDERS = [
     {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
