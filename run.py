bb2412267f707e1b3aaaa1d81cc00504a37ea8db
赖信涛 <laixintao1995@163.com>
2015-09-20 19:19:40 +0800

    correct a clerical error

--- run.py
+++ run.py
@@ -59,9 +59,10 @@ from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHRMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
-app.config['SQLALCHRMY_COMMIT_ON_TEARDOWN'] = True
-
+# app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 db = SQLAlchemy(app)
 
 class Role(db.Model):
