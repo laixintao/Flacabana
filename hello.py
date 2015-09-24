5c94f4568f060de5967093baaf343715807af86e
赖信涛 <laixintao1995@163.com>
2015-09-24 17:09:30 +0800

    login and logout

--- hello.py
+++ hello.py
@@ -2,17 +2,21 @@
 
 import os
 from threading import Thread
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for,request,flash
 from flask.ext.script import Manager, Shell
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms import StringField, SubmitField,PasswordField,BooleanField
+from wtforms.validators import Required,Length,Email
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.migrate import Migrate, MigrateCommand
 # from flask.ext.mail import Mail, Message
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask.ext.login import UserMixin
+from flask.ext.login import LoginManager
+from flask.ext.login import login_required
+from flask.ext.login import login_user,logout_user
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -40,7 +44,7 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
-class User(db.Model):
+class User(db.Model,UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
@@ -102,6 +106,44 @@ def index():
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'),
                            known=session.get('known', False))
+
+# Login
+login_manager = LoginManager(app)
+login_manager.session_protection = "strong"
+login_manager.login_view = 'auth.login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/secret')
+@login_required
+def secret():
+    return "only authenticated users are allowed!"
+
+class LoginForm(Form):
+    id = StringField('User id',validators=[Required(),Length(1,64)])
+    password = PasswordField('Password',validators=[Required()])
+    remember_me = BooleanField('Keep me logged in ')
+    submit = SubmitField('Log in')
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(id=form.id.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user,form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('index'))
+        flash('Invalid username or password!')
+    return render_template('login.html',form = form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out.")
+    return redirect(url_for("index"))
 
 
 if __name__ == '__main__':
